19691371b0e8a939f7e8b2065b8faaf773c27885
Matthias Urlichs <matthias@urlichs.de>
2018-01-28 10:30:20 +0100

    simplified adapters
    no object-with-loop required

--- trio_asyncio/adapter.py
+++ trio_asyncio/adapter.py
@@ -1,49 +1,32 @@
 # This code implements a clone of the asyncio mainloop which hooks into
 # Trio.
 
-import sys
-import trio
-import asyncio
-import math
-import heapq
-import signal
-import threading
-import selectors
+import trio_asyncio
 
-import logging
-logger = logging.getLogger(__name__)
+#import logging
+#logger = logging.getLogger(__name__)
 
 from functools import wraps
-from asyncio.events import _format_callback, _get_function_source
-from selectors import _BaseSelectorImpl, EVENT_READ, EVENT_WRITE
 
 __all__ = ['trio2aio','aio2trio']
 
 
-def trio2aio(loop='loop'):
-    def adapter(proc):
-        @wraps(proc)
-        async def call(self, *args):
-            loop_ = getattr(self, loop)
-            return await loop_.run_asyncio(proc, self, *args)
-        return call
-    if callable(loop):
-        proc,loop = loop,'loop'
-        return adapter(proc)
-    else:
-        return adapter
+def trio2aio(proc):
+    @wraps(proc)
+    async def call(*args):
+        return await trio_asyncio.run_asyncio(proc, *args)
+    return call
 
 
-def aio2trio(loop='loop'):
-    def adapter(proc):
-        @wraps(proc)
-        async def call(self, *args):
-            loop_ = getattr(self, loop)
-            return await loop_.run_trio(proc, self, *args)
-        return call
-    if callable(loop):
-        proc,loop = loop,'loop'
-        return adapter(proc)
-    else:
-        return adapter
+def aio2trio(proc):
+    @wraps(proc)
+    async def call(*args):
+        return await trio_asyncio.run_trio(proc, *args)
+    return call
+
+def aio2trio_task(proc):
+    @wraps(proc)
+    async def call(*args):
+        trio_asyncio.run_trio_task(proc, *args)
+    return call
 
