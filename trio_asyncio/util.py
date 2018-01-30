f8e1222bc28b8cdfba2d6bc40bbdc9da3a834bdc
Matthias Urlichs <matthias@urlichs.de>
2018-01-30 01:14:03 +0100

    more debug

--- trio_asyncio/util.py
+++ trio_asyncio/util.py
@@ -17,30 +17,43 @@ async def run_future(future):
     task = trio.hazmat.current_task()
     raise_cancel = None
 
+    print("RUN_F",future)
     def done_cb(_):
+        print("RUN_F DONE_CB",_)
         trio.hazmat.reschedule(task, trio.hazmat.Result.capture(future.result))
 
     future.add_done_callback(done_cb)
 
     def abort_cb(raise_cancel_arg):
+        print("RUN_F ABORT_CB",raise_cancel_arg)
         # Save the cancel-raising function
         nonlocal raise_cancel
         raise_cancel = raise_cancel_arg
         # Attempt to cancel our future
         future.cancel()
         # Keep waiting
+        print("RUN_F ABORT_CB FAIL")
         return trio.hazmat.Abort.FAILED
 
     try:
-        res = await trio.hazmat.wait_task_rescheduled(abort_cb)
+        print("RUN_F SLEEP")
+        try:
+            res = await trio.hazmat.wait_task_rescheduled(abort_cb)
+        except BaseException as exc:
+            print("RUN_F ERR",exc)
+            raise
+        else:
+            print("RUN_F DONE",res)
         return res
     except asyncio.CancelledError as exc:
-        if raise_cancel is not None:
+        if False and raise_cancel is not None:
             try:
+                print("RUN_F CANCEL RE")
                 raise_cancel()
             finally:
                 # Try to preserve the exception chain for more detailed tracebacks
                 sys.exc_info()[1].__cause__ = exc
         else:
+            print("RUN_F CANCEL PROP")
             raise
 
