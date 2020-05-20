1d1783e0ec4e2403e964a7c13b1d7a2fc7cfc150
Matthias Urlichs <matthias@urlichs.de>
2020-05-20 05:46:41 +0200

    Require pytest-trio 0.6

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     python_requires=">=3.6",  # temporary, for RTD
     keywords=["async", "io", "trio", "asyncio", "trio-asyncio"],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest >= 5.4', 'outcome'],
+    tests_require=['pytest >= 5.4', 'pytest-trio >= 0.6', 'outcome'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
