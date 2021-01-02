d1051b0efa410607e20cf9d2037913104b7f39fa
Matthias Urlichs <matthias@urlichs.de>
2021-01-02 12:03:13 +0100

    Minimal de-`loop=loop`-ification
    required for for Python 3.10+

--- tests/aiotest/test_coroutine.py
+++ tests/aiotest/test_coroutine.py
@@ -17,7 +17,7 @@ class TestCoroutine(aiotest.TestCase):
     async def test_hello_world(self, loop, config):
         result = []
         coro = hello_world(config.asyncio, result, 0.001, loop)
-        await loop.run_aio_coroutine(config.asyncio.ensure_future(coro, loop=loop))
+        await loop.run_aio_coroutine(config.asyncio.ensure_future(coro))
         assert result == ['Hello', 'World']
 
     @pytest.mark.trio
@@ -37,7 +37,7 @@ class TestCoroutine(aiotest.TestCase):
     @pytest.mark.trio
     async def test_waiter(self, loop, config):
         async def waiter(asyncio, hello_world, result):
-            fut = asyncio.Future(loop=loop)
+            fut = asyncio.Future()
             loop.call_soon(fut.set_result, "Future")
 
             value = await fut
