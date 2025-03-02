d74158d83a65a62552eb15279c19757852ddc364
Mike Nerone <mike@nerone.org>
2025-03-02 12:50:50 -0600

    fix: Sphinx no longer requires get_html_theme_path() call
    ...which breaks the docs build.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -132,7 +132,6 @@ todo_include_todos = False
 # html_theme_options.
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
